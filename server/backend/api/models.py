1f2956c32a8c8145a767787e47bc05660b80a988
unknown <ermiasbirhane@gmail.com>
2024-10-08 05:44:47 -0700

    note model migration done

--- server/backend/api/models.py
+++ server/backend/api/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Note(models.Model):
     title = models.CharField(max_length=100)
-    content=model.models.TextField()
+    content=models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='notes')
